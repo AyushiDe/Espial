d195fa777db10982a3ed5008977171517c928500
ayushi <ayushi@minance.com>
2018-06-17 22:57:49 +0530

    image picker done

--- app/src/main/java/com/android/espial/MainActivity.java
+++ app/src/main/java/com/android/espial/MainActivity.java
@@ -1,13 +1,75 @@
 package com.android.espial;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+//    @BindView(R.id.btnImageSelect)
+//    Button btnImageSelect;
+    @BindView(R.id.ivSelectedImage)
+    ImageView ivSelectedImage;
+
+    private final static String TAG = MainActivity.class.getName();
+    private static final int SELECT_PICTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btnImageSelect)
+    void onClickBtnSelect(){
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedFileURI = data.getData();
+                String filepath = getRealPathFromURI(this,selectedFileURI);
+                Bitmap bitmap = BitmapFactory.decodeFile(filepath);
+                Drawable drawable = new BitmapDrawable(bitmap);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ivSelectedImage.setBackground(drawable);
+                }
+                Log.d(TAG, "onActivityResult: "+filepath);
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
 }
